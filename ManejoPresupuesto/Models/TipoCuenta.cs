d339791b8f20f437ea2d7088b5ab4c22ccb36ffd
Miguel Alonso <miguelretamosoin@gmail.com>
2023-12-25 20:09:16 -0500

    Creacion de formulario

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
-        public int Orden { get; set; }fffprueba
+        public int Orden { get; set; }
     }
 }

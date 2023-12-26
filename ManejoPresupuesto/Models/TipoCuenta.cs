e1e1f99253922e38d60dd935474f4487614941ed
Miguel Alonso <miguelretamosoin@gmail.com>
2023-12-25 21:19:58 -0500

    Eliminando datos de prueba para validaciones

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -7,22 +7,9 @@ namespace ManejoPresupuesto.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="El campo {0} es requerido")]
-        [StringLength(maximumLength:50, MinimumLength =3, ErrorMessage ="La longitud del campo {0} debe estar entre {2} y {1}")]
-        [Display(Name ="Nombre del tipo cuenta")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
 
-        /*Probando validaciones por default*/
-        [Required(ErrorMessage ="El campo {0} es requerido")]
-        [EmailAddress(ErrorMessage ="El campo {0} debe ser un correo valido")]
-        public string Email { get; set; }
-        [Range(minimum:18, maximum: 80, ErrorMessage ="El valor de {0} debe estar entre {1} y {2}")]
-        public int Edad { get; set; }
-        [Url(ErrorMessage ="El campo {0} de ser una URL valida")]
-        public string URL { get; set; }
-        [CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
-        [Display(Name = "Tarjeta de Credito")]
-        public string TarjetaDeCredito { get; set; }
     }
 }

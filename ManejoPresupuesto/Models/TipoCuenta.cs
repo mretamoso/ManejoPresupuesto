cf16ff5d15d2b3b399689782a1c4096aaf9d210c
Miguel Alonso <miguelretamosoin@gmail.com>
2023-12-25 20:57:48 -0500

    validaciones adicionales de prueba

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -12,5 +12,17 @@ namespace ManejoPresupuesto.Models
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
+
+        /*Probando validaciones por default*/
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage ="El campo {0} debe ser un correo valido")]
+        public string Email { get; set; }
+        [Range(minimum:18, maximum: 80, ErrorMessage ="El valor de {0} debe estar entre {1} y {2}")]
+        public int Edad { get; set; }
+        [Url(ErrorMessage ="El campo {0} de ser una URL valida")]
+        public string URL { get; set; }
+        [CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
+        [Display(Name = "Tarjeta de Credito")]
+        public string TarjetaDeCredito { get; set; }
     }
 }

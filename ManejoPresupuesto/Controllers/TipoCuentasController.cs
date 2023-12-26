838b52dd192bf2b7df3e30ae2e85589463c299b6
Miguel Alonso <migue@NABURUSS>
2023-12-25 19:18:51 -0500

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Controllers/TipoCuentasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TipoCuentasController: Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
